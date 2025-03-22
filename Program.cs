9037a983c43cc6e999567f27571357555bd67b2d
dofelton <dofelton@gmail.com>
2025-03-22 08:50:35 -0500

    mslearn

--- Program.cs
+++ Program.cs
@@ -1,26 +1,62 @@
-﻿var currentDirectory = Directory.GetCurrentDirectory();
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
+using Newtonsoft.Json;
+
+var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
 
 var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
-Directory.CreateDirectory(salesTotalDir);     
+Directory.CreateDirectory(salesTotalDir);
+
 var salesFiles = FindFiles(storesDirectory);
+var salesData = CalculateSalesTotal(salesFiles);
 
-File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
-
-IEnumerable<string> FindFiles(string folderName)
+double totalSales = 0;
+foreach (var value in salesData.Values)
 {
-    List<string> salesFiles = new List<string>();
+    totalSales += value;
+}
 
-    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+GenerateSalesReport(salesTotalDir, totalSales, salesData);
 
-    foreach (var file in foundFiles)
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{totalSales}{Environment.NewLine}");
+
+IEnumerable<string> FindFiles(string folderName) =>
+    Directory.EnumerateFiles(folderName, "*.json", SearchOption.AllDirectories);
+
+Dictionary<string, double> CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+    var salesData = new Dictionary<string, double>();
+
+    foreach (var file in salesFiles)
     {
-        var extension = Path.GetExtension(file);
-        if (extension == ".json")
-        {
-            salesFiles.Add(file);
-        }
+        string salesJson = File.ReadAllText(file);
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+        salesData[file] = data?.Total ?? 0;
     }
 
-    return salesFiles;
+    return salesData;
 }
+
+void GenerateSalesReport(string directory, double totalSales, Dictionary<string, double> salesData)
+{
+    string reportFilePath = Path.Combine(directory, "sales_summary.txt");
+
+    using (StreamWriter writer = new StreamWriter(reportFilePath))
+    {
+        writer.WriteLine("Sales Summary");
+        writer.WriteLine("----------------------------");
+        writer.WriteLine($"Total Sales: {totalSales.ToString("C", CultureInfo.CurrentCulture)}");
+        writer.WriteLine("\nDetails:");
+
+        foreach (var entry in salesData)
+        {
+            string fileName = Path.GetFileName(entry.Key);
+            writer.WriteLine($"  {fileName}: {entry.Value.ToString("C", CultureInfo.CurrentCulture)}");
+        }
+    }
+}
+
+record SalesData(double Total);
